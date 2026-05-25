9718575fe7bb4ebefca5d198e319fe1c2f8fbfc0
Nguyen Dinh Tri <nguyendinhtri060905@gmail.com>
2026-05-25 13:04:47 +0700

    add cicd pipeline

--- chat_application/src/components/LoginForm.jsx
+++ chat_application/src/components/LoginForm.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
 
-const Modal = () => {
+const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password };
 
     try {
       await axios.get('https://api.chatengine.io/chats', { headers: authObject });
-
       localStorage.setItem('username', username);
       localStorage.setItem('password', password);
-
       window.location.reload();
       setError('');
     } catch (err) {
-      console.log(err);
-      console.log(err.response);
-
-      setError(
-        err.response?.data?.message ||
-        err.message ||
-        'Login failed'
-      );
+      setError('Oops, incorrect credentials.');
     }
   };
 
@@ -49,8 +39,7 @@ const Modal = () => {
         <h1>{error}</h1>
       </div>
     </div>
-
   );
 };
 
-export default Modal;
+export default LoginForm;
